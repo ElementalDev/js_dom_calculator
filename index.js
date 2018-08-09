76d3a279d53c8b6d40706758e596d52b5dee362c
Reece_Collett <rcollett@spartaglobal.com>
2018-08-09 20:33:01 +0100

    Added for loop to add click listeners to all number buttons

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+var numbtns = document.getElementsByClassName("buttonNum");
+var calcScreen = document.getElementById("screen");
 
+for (var i = 0; i < numbtns.length; i++) {
+  numbtns[i].addEventListener("click", function() {
+    calcScreen.innerHTML = "";
+    calcScreen.innerHTML += this.innerHTML;
+  })
+}
+
+function addition(num1, operator, num2) {
+
+}
